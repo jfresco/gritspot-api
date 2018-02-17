a383c8e710bddca6f02119f632b4075bb51f11d8
José Fresco <josemariafresco@gmail.com>
2018-02-17 18:42:47 -0300

    Unit test allocations

--- test/index.js
+++ test/index.js
@@ -1,7 +1,9 @@
 const request = require('supertest')
 const { expect } = require('chai')
+const { times } = require('lodash')
 
 const app = require('../src/app')
+const { Sensors, Workouts } = require('../src/db')
 
 describe('Workouts endpoints', () => {
   describe('GET /workouts', () => {
@@ -42,5 +44,40 @@ describe('Workouts endpoints', () => {
         .get('/workout')
         .expect(404)
     )
+  })
+
+  describe('POST /workout/{id}/allocations', () => {
+    it('allocates sensors', () => {
+      const participants = ['aaa', 'bbb', 'ccc']
+      const availableSensors = Sensors.getAllocatable().slice(0, 3).map(s => s.id)
+      return request(app)
+        .post('/workout/123/allocations')
+        .send({ participants })
+        .expect(200)
+        .then(({ body }) => {
+          // Validate that the response contains the allocations
+          expect(body).to.have.nested.property('workout.allocations').that.is.an('array')
+          times(3, i => {
+            expect(body.workout.allocations[i]).to.have.property('user_id', participants[i])
+            expect(body.workout.allocations[i]).to.have.property('sensor_id', availableSensors[i])
+          })
+
+          // Validate that the DB holds the same data
+          const { attrs } = Workouts.findById('123')
+          expect(body.workout).to.deep.equal(attrs)
+        })
+    })
+
+    it('fails if there are not enough sensors', () => {
+      const availableSensors = Sensors.getAllocatable().map(s => s.id)
+      const participants = times(availableSensors.length + 5, i => `user_${i}`)
+      return request(app)
+        .post('/workout/123/allocations')
+        .send({ participants })
+        .expect(400)
+        .then(({ body }) => {
+          expect(body.error.startsWith('Not enough sensors'))
+        })
+    })
   })
 })
